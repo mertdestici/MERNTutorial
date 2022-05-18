43219789a2ca1e778d2a65ac88c47e68f04ef2ae
mdestici <mdestici@ford.com.tr>
2022-05-18 15:10:25 +0300

    Goal functionality added

--- frontend/src/app/Store.js
+++ frontend/src/app/Store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
-import authReducer from '../features/auth/AuthSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../features/auth/AuthSlice';
+import goalReducer from '../features/goals/GoalSlice';
 
 export const Store = configureStore({
     reducer: {
         auth: authReducer,
+        goals: goalReducer
     },
 })
